3622bfba0bd997ac4ed6c7ed78cf28c30b9bfbc9
nicomanunta <nic.manun@gmail.com>
2024-11-21 15:51:27 +0100

    add fillable for employee and create migration

--- database/migrations/2024_11_21_142110_create_employees_table.php
+++ database/migrations/2024_11_21_142110_create_employees_table.php
@@ -13,6 +13,18 @@ return new class extends Migration
     {
         Schema::create('employees', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->unsignedBigInteger('contract_id');
+            $table->foreign('contract_id')->references('id')->on('contracts')->onDelete('cascade');
+            $table->string('employee_name', 100);
+            $table->string('employee_surname', 100);
+            $table->string('employee_email', 100);->unique();
+            $table->string('employee_phone', 20);->unique();
+            $table->date('employee_birth_date');
+            $table->string('employee_role', 150);
+            $table->enum('employee_status', ['Attivo', 'Sospeso', 'In prova', 'Licenziato', 'Pensione'])->default('Attivo');
+            $table->date('employee_hiring_date');
             $table->timestamps();
         });
     }
