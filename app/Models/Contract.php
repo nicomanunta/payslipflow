525f4601c22c997f18c3d7a0bccb75fe0f863220
nicomanunta <nic.manun@gmail.com>
2024-11-21 17:01:03 +0100

    add fillable and create migration for contract

--- app/Models/Contract.php
+++ app/Models/Contract.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Contract extends Model
 {
-    //
+    protected $fillable = ['employee_id', 'user_id', 'contract_name', 'contract_type', 'contract_level', 'contract_gross_salary', 'contract_weekly_hours', 'contract_vacation_days', 'contract_inps_tax', 'contract_naspi_tax', 'contract_surcharge_municipal', 'contract_start_date', 'contract_end_date'];
 }
--- database/migrations/2024_11_21_142130_create_contracts_table.php
+++ database/migrations/2024_11_21_142130_create_contracts_table.php
@@ -13,6 +13,20 @@ return new class extends Migration
     {
         Schema::create('contracts', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->unsignedBigInteger('employee_id');
+            $table->foreign('employee_id')->references('id')->on('employees')->onDelete('cascade');
+            $table->string('contract_name', 255);
+            $table->set('contract_type', ['Full-Time', 'Part_Time', 'Tempo indeterminato', 'Tempo determinato', 'Stage', 'Lavoro a progetto','Freelance', 'Apprendistato',]);
+            $table->string('contract_level', 50);
+            $table->decimal('contract_gross_monthly_salary', 10, 2)->unsigned();
+            $table->integer('contract_vacation_days')->unsigned()->nullable();
+            $table->decimal('contract_inps_tax', 5, 2)->unsigned()->nullable();
+            $table->decimal('contract_naspi_tax', 5, 2)->unsigned()->nullable();
+            $table->decimal('contract_surcharge_municipal', 5, 2)->unsigned()->nullable();
+            $table->date('contract_start_date');
+            $table->date('contract_end_date')->nullable();
             $table->timestamps();
         });
     }
