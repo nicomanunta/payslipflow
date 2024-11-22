2aa2e1ccfac4bf745041e7f060b498f86592c3f1
nicomanunta <nic.manun@gmail.com>
2024-11-22 12:53:59 +0100

    fix extra overtime

--- app/Models/Extra.php
+++ app/Models/Extra.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Extra extends Model
 {
-    protected $fillable = ['payroll_id', 'extra_overtime_hours', 'extra_thirteenth_salary', 'extra_fourteenth_salary', 'extra_reimbursement_expenses', 'extra_bonus_rewards', 'extra_notes'];
+    protected $fillable = ['payroll_id', 'extra_weekday_overtime_hours', 'extra_weekend_overtime_hours', 'extra_holiday_overtime_hours', 'extra_thirteenth_salary', 'extra_fourteenth_salary', 'extra_reimbursement_expenses', 'extra_bonus_rewards', 'extra_notes'];
 }
